2397c5d5052ec7c5e2d120fada9b1a51b3d7e854
rdjedoui <rafika.djedoui@gmail.com>
2019-08-28 23:07:36 +0200

    Removed redundant Greeting instantiation in GreetingTest 22

--- src/test/java/guru/springframework/GreatingTest.java
+++ src/test/java/guru/springframework/GreatingTest.java
@@ -1,13 +1,17 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreatingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAllMethodes() {
+        System.out.println("---------- Before - I am only called Once!!! ");
+    }
 
     @BeforeEach
     void setUp() {
@@ -18,15 +22,27 @@ class GreatingTest {
 
     @Test
     void helloWorld() {
-
-
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
-
-
         System.out.println(greeting.helloWorld("John"));
     }
+
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each............");
+    }
+
+    @AfterAll
+    static void afterAllMethodes() {
+        System.out.println("---------- After - I am only called Once!!! ");
+    }
+
 }
